565891d2ce00c02428a22e9cbd5ee95898801393
Taylor Gaines <taylorcgaines@gmail.com>
2017-09-15 14:49:35 -0500

    Cleaned up comments.

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const databaseURL = process.env.DATABASE || 'mongodb://localhost:27017/tabletopd
 const Monster = require("./models/monster")
 mongoose.connect(databaseURL);
 
-
-// const port = process.env.PORT || 5000
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`)
-// })
-//api/monsters
 app.get("/monsters/name/:name", (req, res) => {
   console.log('am I crazy?')
   Monster.find({name: req.params.name})
@@ -36,16 +30,6 @@ app.get("/monsters/index/:index", (req, res) => {
   })
 })
 
-// app.get("/api/monsters/:name", (req, res) => {
-//   console.log('am I crazy?')
-//   monsters.find({name: req.params.name})
-//   .then(monster => {
-//     res.json({
-//       monster: monster
-//     })
-//   })
-// })
-//
 // app.get("/api/classes/:name", (req, res) => {
 //   classes.find({name: req.params.name}).sort({createdAt: -1})
 //   .then(classes => {
@@ -54,8 +38,6 @@ app.get("/monsters/index/:index", (req, res) => {
 //     })
 //   })
 // })
-
-
 
 app.listen(3005, function(){
   console.log("here i go! zoom~ http://0.0.0.0:3005")
